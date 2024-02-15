3ed651473734b3fd540e01a719638a39218d056b
sam <samuel.j.peter@gmail.com>
2024-02-15 01:35:34 +0100

    improved layout

--- src/app.py
+++ src/app.py
@@ -18,10 +18,11 @@ LOGGER.setLevel('DEBUG')
 def run():
     page_config()
     st.title(CONFIG.name)
-    st.write(f'Location: {CONFIG.location}')
+    st.write(f'Location: {CONFIG.location}, Timezone: {CONFIG.timezone}')
+    st.write(f'Investment: {CONFIG.volume} CHF, Start: {CONFIG.date}')
 
-    key = 'ammortization'
-    df = pd.DataFrame({key: [0.0] * len(CONFIG.dates)}, index=CONFIG.dates)
+    keys = ('not spent', 'sold')
+    df = pd.DataFrame({key: [0.0] * len(CONFIG.dates) for key in keys}, index=CONFIG.dates)
     for date in df.index:
         # find the slots for the date
         slots = next(s for s in CONFIG.structure if s.first_date <= date <= s.last_date and date.weekday() in s.days_of_week)
@@ -33,15 +34,20 @@ def run():
             end = dt.datetime(date.year, date.month, date.day, hours[i+1]-1, 59, 59, 999, tzinfo=pytz.timezone('CET'))
             data = get_stats(CONFIG.sm_id, start.isoformat(), end.isoformat()).json()
             savings = Statistics(**data).savings_for(CONFIG.tariffs[i%2])
-            df[key][date] += savings
+            for iKey in range(len(keys)):
+                df[keys[iKey]][date] += savings[iKey]
             LOGGER.debug(f'calculated savings for ({date}, {hours[i]}-{hours[i+1]}): {savings} chf, took {time.time() - t:.3f}s')
-    total = df.sum()[key]
     
-    fig = px.bar(df, title=f'{total:.1f} from {CONFIG.volume} ammortized')
-    fig.update_layout(showlegend=False, yaxis_title='Savings [CHF]')
-    st.plotly_chart(fig, use_container_width=True)
-    
+    categories = df.sum()
+    summary = ', '.join([f'{categories[keys[iKey]]:.1f} {keys[iKey]}' for iKey in range(len(keys))])
+    total = categories.sum()
+    st.write('---')
+    st.write(f'ammortization: {total:.1f} CHF ({summary})')
     st.progress(total / CONFIG.volume, f'Expected date of full ammortization: {CONFIG.expected_end(total)}')
+
+    fig = px.bar(df)
+    fig.update_layout(yaxis_title='Savings [CHF]', xaxis_title='Date')
+    st.plotly_chart(fig, use_container_width=True)
 
 
 if __name__ == '__main__':
